22194a355a7af88c60052d7a8b0608135b9ece64
First <pyimcha@cmkl.ac.th>
2024-10-18 21:08:47 +0700

    Added create transactions and budget table

--- Code/account_database.py
+++ Code/account_database.py
@@ -11,17 +11,38 @@ connection = sqlite3.connect("personal_finance.csv")
 
 cursor = connection.cursor()
 
+# Create Account table
 def create_account_table():
-    # SQL command to create a table in the database
-    sql_command = """CREATE TABLE Accounts (
-    account_id INTEGER PRIMARY KEY, 
-    name VARCHAR(64), 
-    category VARCHAR(16), 
-    amount FLOAT
+    sql_command = """CREATE TABLE IF NOT EXISTS Accounts (
+        account_id INTEGER PRIMARY KEY,
+        name VARCHAR(64) NOT NULL,
+        category VARCHAR(16) NOT NULL,
+        balance FLOAT NOT NULL
     );"""
-    
     cursor.execute(sql_command)
 
+# Create Transactions table
+def create_transactions_table():
+    sql_command = """CREATE TABLE IF NOT EXISTS Transactions (
+        transaction_id INTEGER PRIMARY KEY,
+        source_account_id INTEGER NOT NULL,
+        destination_account_id INTEGER NOT NULL,
+        amount FLOAT NOT NULL,
+        date DATE NOT NULL,
+        description TEXT,
+        FOREIGN KEY (source_account_id) REFERENCES Accounts(account_id),
+        FOREIGN KEY (destination_account_id) REFERENCES Accounts(account_id)
+    );"""
+    cursor.execute(sql_command)
+    
+# Create Budget table
+def create_budget_table():
+    sql_command = """CREATE TABLE IF NOT EXISTS Budgets (
+        budget_id INTEGER PRIMARY KEY,
+        category VARCHAR(64) NOT NULL,
+        budgeted_amount FLOAT NOT NULL,
+    );"""
+    cursor.execute(sql_command)
 
-
+connection.commit()
 connection.close()
--- Code/initialize_account.py
+++ Code/initialize_account.py
@@ -1,9 +1,11 @@
-from account_database import create_account_table
+import account_database
 from account import Account
 
 
 def use_template():
-    create_account_table()
+    account_database.create_account_table()
+    account_database.create_transactions_table()
+    account_database.create_budget_table()
     default_accounts = [
         Account(1000, "Account 1", "Income", 0),
         Account(2000, "Account 2", "Expense", 0),
